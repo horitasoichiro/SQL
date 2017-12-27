6280df0299f4d865e4a2d44972152a9fd6a069b7
LIKEIT貸出用 <horitasoichiro@gmail.com>
2017-12-27 18:03:49 +0900

    SQL問題集、回答しました。

---
+++ Q2.sql
@@ -0,0 +1,1 @@
+CREATE TABLE item (item_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,item_name VARCHAR(256)NOT NULL,item_price INT NOT NULL,category_id INT);
---
+++ Q3.sql
@@ -0,0 +1,3 @@
+INSERT INTO item_category (category_name) VALUES ('家具');
+INSERT INTO item_category (category_name) VALUES ('食品');
+INSERT INTO item_category (category_name) VALUES ('本');
