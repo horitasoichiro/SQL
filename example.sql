4a1e1d400e690b1c4b35117931c47d0fcd906bad
LIKEIT貸出用 <horitasoichiro@gmail.com>
2018-01-09 19:02:29 +0900

    サンプルsql

---
+++ example.sql
@@ -0,0 +1,3 @@
+CREATE TABLE employee(id varchar(10) PRIMARY KEY,name varchar(10)DEFAULT NULL,age int);
+insert into employee value('EMP001','佐川真司','20');
+insert into employee value('EMP002','本田圭一','22');
